0cd3ed6935bd393252071ef3df12c8954db81b97
tim48-robot <justintimothy303@gmail.com>
2025-09-15 11:06:55 +0700

    feat: added remove element, no visual adaptation yet

--- scripts.js
+++ scripts.js
@@ -13,21 +13,38 @@ function createAddBook(name, author, arrayName){
 }
 
 createAddBook("The way of making friends", "justin", myLibrary);
-createAddBook("The way of making enemies", "Sun Tzu", myLibrary)
+createAddBook("The way of making enemies", "Sun Tzu", myLibrary);
 
 for (let i=0; i<myLibrary.length; i++){
     let newDiv = document.createElement("div");
     newDiv.className = "bookCard";
-    let span = document.createElement("span");
+    newDiv.setAttribute("data-id", myLibrary[i].id);
+    let title = document.createElement("span");
+    title.className = "weighted";
     let lineBreak = document.createElement("br");
     let authorName = document.createElement("span");
 
-    gridContainer.appendChild(newDiv);
-    span.textContent = myLibrary[i].name;
-    newDiv.appendChild(span);
-    newDiv.appendChild(lineBreak);
-    authorName.textContent = myLibrary[i].author;
-    newDiv.appendChild(authorName);
+    title.textContent = "Title: " +  myLibrary[i].name;
+    authorName.textContent = "Author name: " + myLibrary[i].author;
 
+
+    gridContainer.appendChild(newDiv);
+    newDiv.appendChild(title);
+    newDiv.appendChild(lineBreak);
+    newDiv.appendChild(authorName);
+    newDiv.insertAdjacentHTML('beforeend' , '<svg xmlns="http://www.w3.org/2000/svg" class="svgResize" viewBox="0 0 24 24"><title>delete-outline</title><path d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z" /></svg>');
+
+    const svgClickable = newDiv.querySelector(".svgResize");
+    const bookId = myLibrary[i].id;
+    svgClickable.addEventListener("click", () => {
+        for (let j=0; j<myLibrary.length; j++){
+            if (myLibrary[j].id === bookId){
+                myLibrary.splice(j, 1);
+                newDiv.remove();
+                break;
+            }
+        }
+    });
 }
+
 
